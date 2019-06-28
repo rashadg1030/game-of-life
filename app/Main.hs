5585c23b7fd99dc409188c9c91971284b42ee88f
rashadg1030 <rashad.sasaki@gmail.com>
2019-06-28 00:37:31 -0700

    Add different way to handle mode types

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
-{-# NamedFieldPuns #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
@@ -11,15 +12,18 @@ import qualified Data.Map as Map
 main :: IO ()
 main = runClassic classic
 
-classic :: Classic C.Grid
+classic :: Classic GameState
 classic = Classic
     { title = "Game of Life"
     , rows = sides
     , cols = sides
     , tilePixelSize = 8
     , backgroundColor = Black2
-    , setupFn = return $ GameState { mode = Run
-                                   , grid = C.rPen (sides, sides) }
+    , setupFn = return $ GameState { isPlaying = False
+                                   , isEditing = True
+                                   , grid = C.rPen (sides, sides) 
+                                   , cursor = (0, 0)
+                                   }
     , updateFn = update
     , cleanupFn = const (return ())
     , tileMapFn = tileMap
@@ -27,35 +31,48 @@ classic = Classic
     , quitFn = const False
     }
 
-data Mode = Edit | Play | Pause
 
-data GameState = GameState { mode :: Mode
-                           , grid :: C.Grid }
+data GameState = GameState { isPlaying :: Bool
+                           , isEditing :: Bool 
+                           , grid      :: C.Grid
+                           , cursor    :: (Int, Int) 
+                           }
 
 sides :: Int
 sides = 128
 
-update :: Input -> GameState -> IO GameState
-update i gs = case mode gs of
-                    Edit  -> return $ editing i gs
-                    Play  -> return $ playing i gs 
-                    Pause -> return $ paused i gs 
-  where
-    editing :: Input -> GameState -> GameState
-    editing Input{keys} GameState{grid}
-      | lookupKey keys LeftArrow == Pressed = 
-      | lookupKey keys RightArrow == Pressed = 
-      | lookupKey keys UpArrow == Pressed = 
-      | lookupKey keys DownArrow == Pressed = 
-      | otherwise = g
-    playing :: Input -> GameState -> GameState
-    playing Input{keys} g
-      | 
-    paused :: Input -> GameState -> GameState
-    paused Input{keys} g = 
+insertCell :: (Int, Int) -> C.Cell -> C.Grid -> C.Grid
+insertCell = insert
 
-moveCursor :: Direction -> C.Grid -> C.Grid
-moveCursor
+editing :: Input -> GameState -> GameState
+editing Input{..} gameState@GameState{..} 
+  | lookupKey keys Escape == Pressed    = gameState { isEditing = not isEditing }
+  | lookupKey keys Enter == Pressed     = gameState { grid = insertCell cursor C.Alive  grid }
+  | lookupKey keys Backspace == Pressed = gameState { grid = insertCell cursor C.Dead  grid } 
+  | otherwise                           = gameState { cursor = (x', y') }
+  where
+    x = fst cursor
+    y = snd cursor
+    x'
+      | lookupKey keys LeftArrow  == Pressed = x - 1  
+      | lookupKey keys RightArrow == Pressed = x + 1
+      | otherwise = x
+    y'
+      | lookupKey keys UpArrow   == Pressed = y - 1  
+      | lookupKey keys DownArrow == Pressed = y + 1
+      | otherwise = y
+
+playing :: Input -> GameState -> GameState
+playing Input{keys} g = undefined
+
+
+paused :: Input -> GameState -> GameState
+paused Input{keys} g = undefined
+
+update :: Input -> GameState -> IO GameState
+update input gameState@GameState{..} = return $ case isPlaying of
+    True  -> editing input gameState
+    False -> undefined
 
 cellToTile :: C.Cell -> Tile
 cellToTile C.Alive  = Tile Nothing Nothing (Just wh0)
